3037e6e4091c6cdbfafd19335606d52144df0bcb
Natan Ribeiro <ntnmarcal@gmail.com>
2021-11-03 20:50:04 -0300

    implemented call method of User Profile Search use case and User not found exception

--- lib/data/usecases/user_profile_search.dart
+++ lib/data/usecases/user_profile_search.dart
@@ -1,10 +1,19 @@
+import 'package:git_profile/data/exceptions/user_not_foud_exception.dart';
+import 'package:git_profile/data/http/http_client.dart';
 import 'package:git_profile/domain/entities/user_profile.dart';
 import 'package:git_profile/domain/usecases/user_profile_search.dart';
 
 class UserProfileSearchImpl implements UserProfileSearch{
+  HttpClient client;
+
+  UserProfileSearchImpl({required this.client});
+
   @override
-  Future<UserProfile> call({required String name}) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<UserProfile> call({required String name}) async {
+    Map<String,dynamic> result = await client.getUserProfile(name);
+    if (result.isEmpty){
+      throw UserNotFoundException();
+    }
+    return UserProfile.fromJson(result);
   }
 }
