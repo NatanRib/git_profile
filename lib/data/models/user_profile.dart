e1dced78fd3dd2633a2208ecef2b368924790143
Natan Ribeiro <ntnmarcal@gmail.com>
2021-11-04 18:07:06 -0300

    implemented User Profile Model

---
+++ lib/data/models/user_profile.dart
@@ -0,0 +1,58 @@
+import 'package:git_profile/domain/entities/user_profile.dart';
+
+class UserProfileModel extends UserProfile{
+  
+  UserProfileModel({
+    required name,
+    required login,
+    required bio,
+    required company,
+    required reposUrl,
+    required location,
+    required email,
+    required twitter,
+    required followers,
+    required following
+  }) : super(
+    name: name,
+    login: login,
+    bio: bio,
+    company: company,
+    reposUrl: reposUrl,
+    location: location,
+    email: email,
+    twitter: twitter,
+    followers: followers,
+    following: following
+  );
+  
+  factory UserProfileModel.fromJson(Map<String,dynamic> json){
+    return UserProfileModel(
+      name: json['name'],
+      login: json['login'],
+      bio: json['bio'],
+      company: json['company'], 
+      reposUrl: json['repos_url'], 
+      location: json['location'], 
+      email: json['email'], 
+      twitter: json['twitter'], 
+      followers: json['followers'], 
+      following: json['following']
+    );
+  }
+
+  Map<String,dynamic> toJson(){
+    return {
+      'name': name,
+      'login': login,
+      'bio': bio,
+      'company': company,
+      'repos_url': reposUrl,
+      'location': location,
+      'email': email,
+      'twitter': twitter,
+      'followers': followers,
+      'following': following 
+    };
+  }
+}
--- lib/domain/entities/user_profile.dart
+++ lib/domain/entities/user_profile.dart
@@ -1,3 +1,26 @@
 class UserProfile{
   
+  String name;
+  String login;
+  String bio;
+  String company;
+  String reposUrl;
+  String location;
+  String email;
+  String twitter;
+  int followers;
+  int following;
+
+  UserProfile({
+    required this.name,
+    required this.login,
+    required this.bio,
+    required this.company,
+    required this.reposUrl,
+    required this.location,
+    required this.email,
+    required this.twitter,
+    required this.followers,
+    required this.following
+    });
 }
