44bbaddfbc0d8b546ffce18ea5d6f3c3e5f4bfbb
Natan Ribeiro <ntnmarcal@gmail.com>
2021-11-05 09:28:33 -0300

    Http Client teste - get a user profile json when user profile was founded - green phase (pass)

--- test/infra/http_client_test.dart
+++ test/infra/http_client_test.dart
@@ -6,11 +6,23 @@ import 'package:git_profile/infra/http_client.dart';
 void main() {
 
   test("Should throw a User not found Exception when the query dont return a user profile", () async {
-    //
+    //arrange
     HttpClient httpClient = HttpClientImpl();
     String userNameNotFounded = 'asd7bastcn';
 
     //assert
     expect(httpClient.getUserProfile(userNameNotFounded), throwsA(isA<UserNotFoundException>()));
+  });
+
+  test("Should get user profile json when the query return a user profile from API", () async {
+    //arrange
+    HttpClient httpClient = HttpClientImpl();
+    String userNameFounded = 'natanRib';
+
+    //act
+    Map<String,dynamic> userProfileJson = await httpClient.getUserProfile(userNameFounded);
+  
+    //assert
+    expect(userProfileJson, isA<Map<String,dynamic>>());
   });
 }
