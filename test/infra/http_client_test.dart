20db377077ad16ec15f78ef9046c688d2c37e167
Natan Ribeiro <ntnmarcal@gmail.com>
2021-11-05 08:34:27 -0300

    Http Client teste - throw a user not found exception - red phase

--- test/infra/http_client_test.dart
+++ test/infra/http_client_test.dart
@@ -1,17 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:git_profile/data/exceptions/user_not_foud_exception.dart';
 import 'package:git_profile/data/http/http_client.dart';
 import 'package:git_profile/infra/http_client.dart';
 
 void main() {
 
-  test("Should throw a User not found Exception when the query dont return a user profile", (){
-    //arrange
+  test("Should throw a User not found Exception when the query dont return a user profile", () async {
+    //
     HttpClient httpClient = HttpClientImpl();
     String userNameNotFounded = 'asd7bastcn';
 
-    //act
-    
-
     //assert
+    expect(httpClient.getUserProfile(userNameNotFounded), throwsA(UserNotFoundException));
   });
 }
