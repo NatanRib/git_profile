69b78ef34d0e4ecc0b18ea97dd504cad2c30d389
Natan Ribeiro <ntnmarcal@gmail.com>
2021-11-07 18:47:27 -0300

    HttpClient test - Return repositories json when the user was founded - Red phase

--- test/infra/http_client_test.dart
+++ test/infra/http_client_test.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:git_profile/data/exceptions/user_not_foud_exception.dart';
 import 'package:git_profile/data/http/http_client.dart';
+import 'package:git_profile/data/models/user_profile.dart';
+import 'package:git_profile/domain/entities/user_profile.dart';
 import 'package:git_profile/infra/http_client.dart';
+
+import '../utils/test_objects.dart';
 
 void main() {
 
@@ -24,5 +28,17 @@ void main() {
   
     //assert
     expect(userProfileJson, isA<Map<String,dynamic>>());
+  });
+
+  test("Should get user profile reporitories json when the user was founded", () async {
+    //arrange
+    HttpClient httpClient = HttpClientImpl();
+    UserProfile userFounded = UserProfileModel.fromJson(correctAPIReturn);
+
+    //act
+    List<Map<String,dynamic>> userRepositoriesJson = await httpClient.getRepositoriesFromUser(userFounded);
+  
+    //assert
+    expect(userRepositoriesJson, isA<List<Map<String,dynamic>>>());
   });
 }
